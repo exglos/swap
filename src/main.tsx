c940ae65ee169bc9557d835ead21d2659a89ab0d
naftalimurgor <murgornaftali@gmail.com>
2026-02-28 15:31:35 +0300

    improve notification

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Toaster } from 'sonner'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Toaster 
+      theme="dark" 
+      position="top-right" 
+      richColors 
+      closeButton 
+      toastOptions={{
+        style: { background: '#1a1b1f', border: '1px solid #2d2e33', color: '#fff' },
+      }}
+    />
   </StrictMode>,
 )
